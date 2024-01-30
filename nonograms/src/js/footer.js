233a11893794bcb0d612946366ac707308ce6df4
Izy <126877709+izy-code@users.noreply.github.com>
2024-01-30 19:17:05 +0300

    feat: add footer layout

---
+++ nonograms/src/js/footer.js
@@ -0,0 +1,13 @@
+import { createNode } from './util';
+
+const renderFooter = () => {
+  const footerNode = createNode(document.body, 'footer', 'footer');
+
+  const randomButtonNode = createNode(footerNode, 'button', 'footer__button', 'Random game', { type: 'button' });
+  const solutionButtonNode = createNode(footerNode, 'button', 'footer__button', 'Solution', { type: 'button' });
+  const timerNode = createNode(footerNode, 'span', 'footer__timer', '00:00');
+  const saveButtonNode = createNode(footerNode, 'button', 'footer__button', 'Save game', { type: 'button' });
+  const continueButtonNode = createNode(footerNode, 'button', 'footer__button', 'Continue saved game', { type: 'button' });
+};
+
+export { renderFooter };
